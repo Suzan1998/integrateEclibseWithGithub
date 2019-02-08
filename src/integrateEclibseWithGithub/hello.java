2da1e76980e0b2499b2b09526614b483f822adc4
Suzan Kamal <Suzan Kamal@DESKTOP-7TA6N9K>
2019-02-08 18:33:33 +0200

    **

--- src/integrateEclibseWithGithub/hello.java
+++ src/integrateEclibseWithGithub/hello.java
@@ -3,7 +3,7 @@ package integrateEclibseWithGithub;
 public class hello {
 public static void main(String[] args) {
 	System.out.println("HELLO WORLD");
-	System.out.println("HELLO SUZAN");
+	System.out.println("HELLO SUZAN *****");
 
 }
 }
